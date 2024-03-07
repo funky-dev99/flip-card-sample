162be376206fb285d15170f7e6d4801407c0f6ea
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2024-03-07 10:53:41 +0530

    Updated

--- flip_card_sample/lib/main.dart
+++ flip_card_sample/lib/main.dart
@@ -17,47 +17,77 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key,});
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Flip Card Demo'),
-      ),
-      body: Center(
-        child: FlipCard(
-          front: Container(
-            width: 300,
-            height: 200,
-            color: Colors.blue,
-            child: Center(
-              child: Text('Front', style: TextStyle(fontSize: 24, color: Colors.white)),
-            ),
-          ),
-          back: Container(
-            width: 300,
-            height: 200,
-            color: Colors.red,
-            child: Center(
-              child: Text('Back', style: TextStyle(fontSize: 24, color: Colors.white)),
-            ),
-          ),
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('Flutter Flip Card Demo')),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FlipCard(
+                front: Container(
+                  width: 300,
+                  height: 200,
+                  color: Colors.blue,
+                  child: Center(
+                    child: Text('Front',
+                        style: TextStyle(fontSize: 24, color: Colors.white)),
+                  ),
+                ),
+                back: Container(
+                  width: 300,
+                  height: 200,
+                  color: Colors.red,
+                  child: Center(
+                    child: Text('Back',
+                        style: TextStyle(fontSize: 24, color: Colors.white)),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              FlipCard(
+                front: Container(
+                  width: 300,
+                  height: 300,
+                  child: Image.asset(
+                    'images/forApp.png', // Replace with your asset path for the front image
+                    fit: BoxFit
+                        .cover, // Ensures the image covers the container fully
+                  ),
+                ),
+                back: Container(
+                  width: 300,
+                  height: 300,
+                  child: Image.asset(
+                    'images/forAppBack.png', // Replace with your asset path for the back image
+                    fit: BoxFit
+                        .cover, // Ensures the image covers the container fully
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
